0329281c1086fd4bc9f71d9f054a29ce03078a88
vmchawla <vmchawla158kb@hotmail.com>
2017-08-18 17:37:36 +0530

    Added waypoints
    Got the enemy to move from Start to End using Waypoints

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform[] _wayPointsTransforms;
+    [SerializeField] private float _speed = 5f;
+
+    private int _targetWayPoint = 0;
+ 
+
+    void Awake()
+    {
+        Assert.IsNotNull(_wayPointsTransforms);
+    }
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_targetWayPoint < _wayPointsTransforms.Length)
+        {
+            Vector3 dir = _wayPointsTransforms[_targetWayPoint].position - transform.position;
+            transform.Translate(dir.normalized * _speed * Time.deltaTime, Space.World);
+        }
+
+        
+
+    }
+
+
+
+
+
+void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "waypoint")
+        {
+            _targetWayPoint++;
+            if (_targetWayPoint >= _wayPointsTransforms.Length)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
+
+
